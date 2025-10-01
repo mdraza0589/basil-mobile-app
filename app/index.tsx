ed43c696ea1003ba40b51749ba85a60c88489a60
mdraza0589 <razamohammad0589@gmail.com>
2025-10-01 18:06:23 +0530

    index and screen file change

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, router } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
@@ -8,9 +8,9 @@ export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
       <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
+      <TouchableOpacity onPress={() => router.push('/intro/screen1')} style={styles.link}>
+        <Text>Go to home screen</Text>
+      </TouchableOpacity>
     </ThemedView>
   );
 }
--- app/intro/screen1.tsx
+++ app/intro/screen1.tsx
@@ -1,40 +1,19 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
-import { useEffect } from 'react';
 
 export default function Screen1() {
   const router = useRouter();
 
-  // Automatically navigate after 3 seconds
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('../intro/screen2');
-    }, 3000); // 3000ms = 3s
-
-    // Cleanup in case component unmounts before 3s
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <View style={styles.container}>
-      {/* Logo */}
-      <Image
-        source={require('../../assets/images/logo/basillog.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />
-
+      <Text style={styles.title}>Welcome to the App!</Text>
+      <Button title="Next" onPress={() => router.push('../intro/screen2')} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#123530', // Page background color
-  },
-  logo: { width: 150, height: 150, marginBottom: 20 },
-  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20, color: '#ffffff' }, // Text color
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
 });
+
